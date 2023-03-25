9e665a8b70ac4b06403e57d34b9eba963258030e
Kozak Liubomyr <liubomyr.kozak@gmail.com>
2023-03-25 16:17:57 +0200

    change consumer

--- TeacherBot.js
+++ TeacherBot.js
@@ -21,35 +21,23 @@ bot.action('BOT_start_lecture', ctx => {
 });
 
 
-let gueue = [];
-
 function checkQueue(teacherData) {
   AMPQConnection.consume(function (msg) {
-    gueue.push(msg)
+    const msgBody = JSON.parse(msg.content.toString());
+    console.log("checkQueue:: -> msgBody", msgBody);
+    if (msgBody.content.includes('Студетнт присутній')) {
+      const studentName = msgBody.sender.first_name;
+
+      AMPQConnection.ack(msg);
+
+      bot.telegram.getMe().then(() => {
+        const teacherMessage = 'Вітає з почаком лекції';
+
+        bot.telegram.sendMessage(teacherData.id, `Студент ${studentName} в мережі`);
+        studentBot.telegram.sendMessage(msgBody.senderId, `Викладач ${teacherData.first_name} почав віддалену пару і ${teacherMessage}`);
+      });
+    }
   });
-
-  setInterval(() => {
-    const oldQueue = [...gueue];
-    gueue = [];
-
-    oldQueue.forEach((msg) => {
-      const msgBody = JSON.parse(msg.content.toString());
-      console.log("checkQueue:: -> msgBody", msgBody);
-      if (msgBody.content.includes('Студетнт присутній')) {
-        const studentName = msgBody.sender.first_name;
-
-        AMPQConnection.ack(msg);
-
-        bot.telegram.getMe().then(() => {
-          const teacherMessage = 'Вітає з почаком лекції';
-
-          bot.telegram.sendMessage(teacherData.id, `Студент ${studentName} в мережі`);
-          studentBot.telegram.sendMessage(msgBody.senderId, `Викладач ${teacherData.first_name} почав віддалену пару і ${teacherMessage}`);
-        });
-      }
-    })
-
-  }, 3 * 60 * 1000);
 }
 
 bot.start((ctx) => {
